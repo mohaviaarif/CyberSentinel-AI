643a0b3381969b04d647c0407e2483dbfcdc6a63
mohaviaarif <mohaviaarif693@gmail.com>
2025-11-28 09:27:09 +0500

    Fix .gitignore

---
+++ backend/test-env.py
@@ -0,0 +1,5 @@
+import sys, flask, pandas, sklearn
+print("Python path:", sys.executable)
+print("Flask:", flask.__version__)
+print("Pandas:", pandas.__version__)
+print("Sklearn:", sklearn.__version__)
